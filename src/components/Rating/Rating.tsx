02f4376a7f21c535bc4a6e7afecc066d020a3159
enrouxlibre <enrouxlibre23@gmail.com>
2026-01-13 14:05:42 +0100

    Changed image path in Rating and fixed function name

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,15 +1,15 @@
 import "./Rating.scss";
+import star from "@images/star.svg";
+import greystar from "@images/star-grey.svg";
 
-export default function Thumbnail({ rating }: { rating: number }) {
+export default function Rating({ rating }: { rating: number }) {
   return (
     <div className="rating">
       {[...Array(5)].map((_, index) => {
         if (index < rating) {
-          return <img src="/images/star.svg" alt="star" key={index} />;
+          return <img src={star} alt="star" key={index} />;
         } else {
-          return (
-            <img src="/images/star-grey.svg" alt="grey star" key={index} />
-          );
+          return <img src={greystar} alt="grey star" key={index} />;
         }
       })}
     </div>
