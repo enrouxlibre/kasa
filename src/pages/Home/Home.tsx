0d1283bd70bb2fe7cc5eb528a38d1b33e17332f4
enrouxlibre <enrouxlibre23@gmail.com>
2026-01-19 14:30:55 +0100

    Added fetch to Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,43 @@
 import "./Home.scss";
 import cliffImage from "@images/cliff.png";
 import TitleImage from "../../components/TitleImage/TitleImage";
-import logements from "../../assets/logements.json";
+// import logements from "../../assets/logements.json";
 import Thumbnail from "../../components/Thumbnail/Thumbnail";
+import { useEffect, useState } from "react";
+
+export interface logementType {
+  id: string;
+  title: string;
+  cover: string;
+  pictures: string[];
+  description: string;
+  host: {
+    name: string;
+    picture: string;
+  };
+  rating: string;
+  location: string;
+  equipments: string[];
+  tags: string[];
+}
 
 export default function Home() {
+  const [logements, setLogements] = useState<logementType[]>([]);
+  useEffect(() => {
+    fetch("http://localhost:5173/logements.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Not ok");
+        }
+        return response.json();
+      })
+      .then((result) => {
+        setLogements(result);
+      })
+      .catch((err) => {
+        console.error("Fetch error: ", err);
+      });
+  }, []);
   const logement_list = logements.map((logement) => (
     <li key={logement.id}>
       <a href={"/housing/" + logement.id}>
